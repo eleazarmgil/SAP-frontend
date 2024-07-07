28e8f5735dbdc63933b0208648cd46a2435a9d1e
eleazarmgil <maug.lucas@gmail.com>
2024-07-07 00:31:17 -0400

    Added landing-page and updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from 'node_modules/@angular/router';
 import { BaseComponent } from './components/layout/base/base.component';
 import { ErrorPageComponent } from './components/views/error-page/error-page.component';
+import { MainPageComponent } from './components/views/landing-page/main-page/main-page.component';
+import { AboutComponent } from './components/views/landing-page/about/about.component';
 
 const routes: Routes = [
-  //{ path: '', loadChildren: () => import('src/app/views/pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: '', component: MainPageComponent },
+  { path: 'about', component: AboutComponent},
   {
     path: 'app',
     component: BaseComponent,
