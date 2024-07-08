de38cd281e2ca034dc279464214a1dfdc3927e57
eleazarmgil <maug.lucas@gmail.com>
2024-07-08 04:48:03 -0400

    Added main page of landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from 'node_modules/@angular/router';
-import { BaseComponent } from './components/layout/base/base.component';
 import { ErrorPageComponent } from './components/views/error-page/error-page.component';
+import { LandingBaseComponent } from './components/views/landing-page/layout/base/landing-base.component';
 import { MainPageComponent } from './components/views/landing-page/main-page/main-page.component';
 import { AboutComponent } from './components/views/landing-page/about/about.component';
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent },
-  { path: 'about', component: AboutComponent},
   {
-    path: 'app',
-    component: BaseComponent,
+    path: '',
+    component: LandingBaseComponent,
     children: [
-      // {
-      //   path: 'profile',
-      //   loadChildren: () => import('./views/pages/profile/profile.module').then(m => m.ProfileModule)
-      // },
-      // {
-      //   path: 'explore',
-      //   loadChildren: () => import('src/app/views/pages/explore/explore.module').then(m => m.ExploreModule)
-      // },
-      // {
-      //   path: 'email',
-      //   loadChildren: () => import('src/app/views/pages/email/email.module').then(m => m.EmailModule)
-      // },
-      // {
-      //   path: 'products',
-      //   loadChildren: () => import('src/app/views/pages/products/products.module').then(m => m.ProductsModule)
-      // },
-      // {
-      //   path: 'purchases',
-      //   loadChildren: () => import('src/app/views/pages/purchases/purchases.module').then(m => m.PurchasesModule)
-      // }
+      {
+        path: '',
+        component: MainPageComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
     ]
   },
   {
