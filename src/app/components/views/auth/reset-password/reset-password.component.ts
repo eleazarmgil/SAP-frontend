a9e8adbc4dabc01720fb4c6dfc89c810b6a5dfb7
eleazarmgil <maug.lucas@gmail.com>
2024-07-19 01:04:06 -0400

    Modified styles for reset-password

--- src/app/components/views/auth/reset-password/reset-password.component.ts
+++ src/app/components/views/auth/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Lógica para procesar el formulario
+    }
+  }
 }
