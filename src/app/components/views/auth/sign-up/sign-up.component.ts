30454eb6be255348e9a93a29ced78cd03f299994
eleazarmgil <maug.lucas@gmail.com>
2024-07-20 19:30:48 -0400

    Added form for signup page

--- src/app/components/views/auth/sign-up/sign-up.component.ts
+++ src/app/components/views/auth/sign-up/sign-up.component.ts
@@ -7,21 +7,41 @@ import { lastIndexOf } from 'cypress/types/lodash';
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
+
+
 export class SignUpComponent {
+  isPsychologist: boolean = false;
   signUpForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.signUpForm = this.formBuilder.group({
-      firstName:['', [Validators.required]],
-      lastName:['', [Validators.required]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required, this.confirmPasswordValidator.bind(this)]],
     });
+  }
+
+  confirmPasswordValidator(control: FormGroup): { [s: string]: boolean } | null {
+    if (!control || !control.parent) {
+      return null;
+    }
+    const password = control.parent?.get('password')?.value;
+    const confirmPassword = control.value;
+
+    if (confirmPassword !== password) {
+      return { passwordsNotMatch: true };
+    }
+
+    return null;
   }
 
   onSubmit() {
     if (this.signUpForm.valid) {
-      // Lógica para procesar el formulario
+      console.log("Form valido "+this.isPsychologist);
+    }else{
+      console.error("Form not valid")
     }
   }
 }
