48c6399cb910eb71885b7ba11e5a139aee355223
eleazarmgil <maug.lucas@gmail.com>
2024-10-20 01:45:40 -0400

    Added components

---
+++ src/app/components/views/app/psychologist/profile/update-credentials/update-credentials.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-credentials',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-credentials.component.html',
+  styleUrl: './update-credentials.component.css'
+})
+export class UpdateCredentialsComponent {
+
+}
