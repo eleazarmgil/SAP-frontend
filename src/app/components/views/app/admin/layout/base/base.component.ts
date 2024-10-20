c832ec15ce99a974fa3e354bb0fb8f2abfffecb7
eleazarmgil <maug.lucas@gmail.com>
2024-10-20 06:04:57 -0400

    Added sidebar styles and links

--- src/app/components/views/app/admin/layout/base/base.component.ts
+++ src/app/components/views/app/admin/layout/base/base.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from 'node_modules/@angular/router';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminSidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-admin-base',
   templateUrl: './base.component.html',
   styleUrls: ['./base.component.scss']
 })
-export class AdminBaseComponent implements OnInit {
+export class AdminBaseComponent implements OnInit, AfterViewInit {
 
-  constructor(private router: Router) {
+  @ViewChild(AdminSidebarComponent) adminSidebar!: AdminSidebarComponent;
+  isSidebarRetracted: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    // Inicializa el estado
+    this.isSidebarRetracted = this.adminSidebar.isSidebarRetracted;
   }
-  isSidebarRetracted = false;
 
-  onSidebarToggle(retracted: boolean): void {
-    this.isSidebarRetracted = retracted;
+  onSidebarStateChange(retracted: boolean): void {
+    this.isSidebarRetracted = retracted; // Actualiza el estado
   }
-
-  ngOnInit(): void {
-  }
-
 }
