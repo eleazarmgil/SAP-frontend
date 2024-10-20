380f4053c2b081dc28013a5bcc7d7909458e9ef6
Eleazar Gil <92376765+eleazarmgil@users.noreply.github.com>
2024-10-20 01:47:40 -0400

    Added basic components (#2)
    * Added basic components

---
+++ src/app/components/views/app/admin/users/read-user/read-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './read-user.component.html',
+  styleUrl: './read-user.component.css'
+})
+export class ReadUserComponent {
+
+}
