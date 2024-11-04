519ced9a51035d35fe9bd02ebf60d884cfa51e2f
eleazarmgil <maug.lucas@gmail.com>
2024-11-04 17:47:48 -0400

    Added update-password component

--- src/app/components/views/app/admin/profile/reset-password/reset-password.component.ts
+++ src/app/components/views/app/admin/profile/reset-password/reset-password.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-reset-password',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.css'
+  styleUrl: './reset-password.component.scss'
 })
 export class AdminResetPasswordComponent {
+
 
 }
