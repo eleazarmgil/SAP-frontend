4d19e649d95650a043336edb6fd422c4e2c4ae9d
eleazarmgil <maug.lucas@gmail.com>
2024-11-04 18:17:43 -0400

    Added update username

--- src/app/components/views/app/admin/profile/update-username/update-username.component.ts
+++ src/app/components/views/app/admin/profile/update-username/update-username.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-update-username',
+  selector: 'app-admin-update-username',
   standalone: true,
   imports: [],
   templateUrl: './update-username.component.html',
-  styleUrl: './update-username.component.css'
+  styleUrl: './update-username.component.scss'
 })
 export class AdminUpdateUsernameComponent {
 
