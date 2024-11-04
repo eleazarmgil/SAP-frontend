08649afe6d0b34014210ede1c6911313d9c4d905
eleazarmgil <maug.lucas@gmail.com>
2024-11-04 04:06:18 -0400

    Updated update-email component

--- src/app/components/views/app/admin/profile/update-email/update-email.component.ts
+++ src/app/components/views/app/admin/profile/update-email/update-email.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-update-email',
+  selector: 'app-admin-update-email',
   standalone: true,
   imports: [],
   templateUrl: './update-email.component.html',
-  styleUrl: './update-email.component.css'
+  styleUrl: './update-email.component.scss'
 })
 export class AdminUpdateEmailComponent {
 
