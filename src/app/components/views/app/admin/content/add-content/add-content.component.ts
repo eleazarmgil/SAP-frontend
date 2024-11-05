aaa692a2f3c09ab77743afc5301447b065c450cf
eleazarmgil <maug.lucas@gmail.com>
2024-11-04 23:09:23 -0400

    Added styles

---
+++ src/app/components/views/app/admin/content/add-content/add-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-content.component.html',
+  styleUrl: './add-content.component.scss'
+})
+export class AddContentComponent {
+
+}
