fa3d01200626a152e03d67916721405770b5a432
eleazarmgil <maug.lucas@gmail.com>
2024-11-24 02:27:03 -0400

    Updated file distribution

---
+++ src/app/pages/profile/update-username/update-username.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-username',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-username.component.html',
+  styleUrl: './update-username.component.scss'
+})
+export class UpdateUsernameComponent {
+
+}
