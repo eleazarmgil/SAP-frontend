7c4d78d1e13c3b57791919a47935e25f7602431b
eleazarmgil <maug.lucas@gmail.com>
2024-11-24 04:50:06 -0400

    Fixed sidebar for admin component

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { User } from '../../../core/models/login-response.model';
+
+interface MenuItem {
+  label: string;
+  link: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+  styleUrl: './admin.component.scss',
 })
 export class AdminComponent {
+  public isSidebarRetracted = false;
+  showUsername = true;
+  user: User | null = JSON.parse(localStorage.getItem('user') || 'null');
 
+  // Combina nombre y apellido
+  name: string = this.user ? `${this.user.nombre} ${this.user.apellido}` : 'Invitado';
+
+  menuItems: MenuItem[] = [
+    { label: 'Perfil', link: '/admin/profile', icon: 'settings' },
+    { label: 'Control de Usuarios', link: '/admin/users', icon: 'user' },
+    { label: 'Control de Contenido', link: '/admin/content', icon: 'settings' },
+    { label: 'Cerrar Sesión', link: '/logout', icon: 'log-out' },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleSidebar(): void {
+    this.isSidebarRetracted = !this.isSidebarRetracted;
+  }
 }
