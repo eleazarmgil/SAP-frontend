445cd1d603b4de327cbb386915e4c3927e8a31e3
eleazarmgil <maug.lucas@gmail.com>
2024-11-24 04:50:06 -0400

    Fixed sidebar for admin component

--- src/core/models/login-response.model.ts
+++ src/core/models/login-response.model.ts
@@ -3,13 +3,13 @@ export interface LoginResponse {
   isSuccess: boolean;
   errorMessages: string[];
   result: {
-    usuario: Usuario;
+    usuario: User;
     token: string;
     role: string;
   };
 }
 
-export interface Usuario {
+export interface User {
   id: string;        // UUID del usuario
   email: string;     // Correo electrónico del usuario
   nombre: string;    // Nombre del usuario
