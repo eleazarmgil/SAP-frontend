3723c1cf5a91a24a24623a37e158f734a295e176
eleazarmgil <maug.lucas@gmail.com>
2024-11-28 02:35:17 -0400

    Added filters to users table and improved styles

--- src/app/pages/user-control/list-user/list-user.component.ts
+++ src/app/pages/user-control/list-user/list-user.component.ts
@@ -4,17 +4,20 @@ import { UserControlService } from '../../../../core/services/user-control/user-
 import { User } from '../../../../core/models/user.model';
 import { NavigationService } from '../../../../core/services/navigation/navigation.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-list-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './list-user.component.html',
   styleUrl: './list-user.component.scss'
 })
 export class ListUserComponent {
   users: User[] = [];
+  filteredUsers: User[] = []; // Usuarios filtrados
+  searchTerm: string = ''; // Término de búsqueda
 
   constructor(private router: Router, private userService:UserControlService){}
 
@@ -30,7 +33,7 @@ export class ListUserComponent {
     this.userService.getAllUsers().subscribe({
       next: (data: User[]) => {
         this.users = data; // Asigna la respuesta a la propiedad users
-        console.log('Usuarios obtenidos:', this.users);
+        this.filteredUsers = data; // Inicializa los usuarios filtrados con todos los usuarios
       },
       error: (error: any) => { // Especifica el tipo como 'any'
       }
@@ -41,8 +44,17 @@ export class ListUserComponent {
     this.router.navigate(['/app/users/'+id])
   }
 
-  deleteUser(userId: String): void {
-    this.users = this.users.filter(user => user.id !== userId);
-    console.log('Usuario eliminado con ID:', userId);
+  filterUsers(): void {
+    const term = this.searchTerm.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Normaliza y elimina acentos
+    if (!term) {
+      this.filteredUsers = this.users; // Si no hay término de búsqueda, mostrar todos los usuarios
+    } else {
+      this.filteredUsers = this.users.filter(user =>
+        user.nombre.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(term) ||
+        user.apellido.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(term) ||
+        user.role?.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(term)
+      );
+    }
   }
+
 }
